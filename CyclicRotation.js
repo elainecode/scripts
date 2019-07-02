0d2b1768dd648d1bf30284d098d7a39c836776b3
elaine <remrow@outlook.com>
2019-07-02 14:13:43 -0700

    added script

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -6,3 +6,15 @@ function rotate(num, array){
   }
   return array
 }
+
+function rotate(num, array){
+ let len =  array.length
+ let count = num % len
+ const result = []
+  for(let i = 0; i < len; i++) {
+  result[(i + count) % len] = array[i]
+  }
+  return result
+}
+
+rotate(3, [1,2,3,4,5,6,7,8,9,10])
