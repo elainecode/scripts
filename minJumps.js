81172e6749f6a87a0a9490dcf2e008a23bc5404a
elaine <remrow@outlook.com>
2019-07-06 19:07:02 -0700

    new solution

--- minJumps.js
+++ minJumps.js
@@ -1,7 +1,14 @@
 
 function frogJump(X, Y, D) {
+   
+   //solution with better time complexiy:
+   // Let J equal number of jumps
+   // Y =  X + (J * D)
+   // J = (Y - X)/D 
+   //return Math.ceil((Y - X )/ D)
+
    let curr = X
-    let count = 0
+   let count = 0
     while(curr < Y) {
         curr += D
         count++
